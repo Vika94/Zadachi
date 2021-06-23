193dc12eed6933486170e0902b60802efa8a511d
Vika94 <vikuscha1994@mail.ru>
2021-06-23 22:09:49 +0300

    Initial commit

---
+++ src/MyZadachki.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class MyZadachki {
+    public static void main(String[] args) {
+      /*  //Задача 1//
+        System.out.print("Введите число: ");
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        if (num1 > -10 && num1 < 0)
+            System.out.println("Это однозначное отрицательное число ");
+        else if (num1 > -100 && num1 < -10)
+            System.out.println("Это двухзначное отрицательное число ");
+        else if (num1 > -1000 && num1 < -100)
+            System.out.println("Это трехзначное отрицательное число ");
+        else if (num1 < 10 && num1 > 0)
+            System.out.println("Это однозначное положительное число ");
+        else if (num1 < 100 && num1 >= 10)
+            System.out.println("Это двухзначное положительное число ");
+        else if (num1 < 1000 && num1 >= 100)
+            System.out.println("Это трехзначное положительное число ");
+*/
+       /* //Задача 2//
+        System.out.print("Введите сторону треугольника a: ");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.print("Введите сторону треугольника b: ");
+        Scanner scan1 = new Scanner(System.in);
+        int b = scan1.nextInt();
+        System.out.print("Введите сторону треугольника c: ");
+        Scanner scan2 = new Scanner(System.in);
+        int c = scan2.nextInt();
+        if (a + b < c || a + c < b || b + c < a)
+            System.out.println("Треугольник не существует");
+        else if (a + b > c && a + c > b && b + c > a)
+            System.out.println("Треугольник существует");
+        */
+      /*  //Задача 3//
+        System.out.print("Введите целое число: ");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        if (a > 0)
+            System.out.println("Равно" + " " + (a = a + 1));
+        else if (a == 0)
+            System.out.println("Равно" + " " + (a = 10));
+        else if (a < 0)
+            System.out.println("Равно" + " " + (a = a - 2));
+       */
+        //Задача 4.Вывод четных чисел от 0 до 100//
+       /* for (int i = 2; i <= 100; i = i + 2) {
+            System.out.print(" " + i + " ");
+
+        */
+        //Задача 5. Сумма нечетных//
+       /* int sum = 0;
+        for (int i = 1; i <= 99; i = i + 2) {
+            System.out.print("+" + i);
+            sum = sum + i;
+        }
+        System.out.print("=" + sum);
+        
+        */
+
+
+    }
+}
+
+
+
+
